d64e8985d05b29a23643fbb7590740dc19aaae80
Sergey Ponomarev <stokito@gmail.com>
2023-08-20 21:28:05 +0300

    Build OpenJDK 8 as OPKG

--- dljdk8.sh
+++ dljdk8.sh
@@ -1,13 +1,11 @@
-#!/bin/sh
-
+#!/bin/bash
 set -o errexit
 set -o nounset
-
 #set -o pipefail
 set -x
 
-REVISION="8.282.08-r1"
-URL=http://dl-cdn.alpinelinux.org/alpine/v3.14/community
+REVISION="8.275.01-r0"
+URL="https://dl-cdn.alpinelinux.org/alpine/v3.12/community"
 ARCH="aarch64 armhf armv7 ppc64le s390x x86 x86_64"
 PACKAGES="openjdk8 openjdk8-jre openjdk8-jre-lib openjdk8-jre-base"
 
@@ -17,38 +15,30 @@ trap "rm -rf $tmp_dir" EXIT
 
 cd "${tmp_dir}"
 
-#download packages
-for arch in $ARCH;do
+for arch in $ARCH; do
+	mkdir -p "openjdk8-${arch}/data/"
+	cp -r "$old_pwd/build/control/" "$old_pwd/build/debian-binary" "openjdk8-${arch}/"
 	for package in $PACKAGES; do
-		curl -o "${package}-${REVISION}_${arch}.apk" "${URL}/${arch}/${package}-${REVISION}.apk"
+		# download packages
+		wget -O "${package}-${REVISION}_${arch}.apk" "${URL}/${arch}/${package}-${REVISION}.apk"
+		# extract apks to corresponding arch dir
+		tar xzf "${package}-${REVISION}_${arch}.apk" -C "openjdk8-${arch}/data/" --exclude=.PKGINFO --exclude=.SIGN*
 	done
-done 
-
-#mkdir
-for arch in $ARCH;do
-	mkdir "openjdk8-${arch}"
-done
-
-#extract apks to corresponding arch dir
-for arch in $ARCH;do
-	for package in $PACKAGES; do
-		#tar xzf "${package}-${REVISION}.apk"
-		tar xzf "${package}-${REVISION}_${arch}.apk" -C "openjdk8-${arch}"
-	done
-done
-
-for arch in $ARCH;do
-	chmod +x "openjdk8-${arch}/usr/lib/jvm/java-1.8-openjdk/bin/" 
-done
-
-#tar them up agains
-for arch in $ARCH;do
-	tar czf "openjdk-8_${arch}.tar.gz" -C "openjdk8-${arch}/usr/lib/jvm/java-1.8-openjdk/" .
+	chmod +x "openjdk8-${arch}/data/usr/lib/jvm/java-1.8-openjdk/bin/"
+	# tar them into opkg package
+	pushd "./openjdk8-${arch}/control/"
+	tar --numeric-owner --group=0 --owner=0 -czf ../control.tar.gz ./*
+	popd
+	pushd "./openjdk8-${arch}/data/"
+	tar --numeric-owner --group=0 --owner=0 -czf ../data.tar.gz ./*
+	popd
+	pushd "./openjdk8-${arch}/"
+	tar --numeric-owner --group=0 --owner=0 -czf "./openjdk8_$REVISION.$arch.ipk" ./debian-binary ./data.tar.gz ./control.tar.gz
+	popd
 done
 
 cd "${old_pwd}"
 
 for arch in $ARCH;do
-	cp "$tmp_dir/openjdk-8_${arch}.tar.gz" "./"
+	cp "$tmp_dir/openjdk8-${arch}/openjdk8_$REVISION.$arch.ipk" ./
 done
-
